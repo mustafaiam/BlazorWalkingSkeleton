8021eb56b6b4a76cc7ac486abaacd86863e01153
mustafaiam <mustafaiam.github@gmail.com>
2022-04-09 21:11:02 +0300

    Minor changes
    Made some minor changes. I will continue later.

---
+++ BlazorWalkingSkeleton/BlazorWalkingSkeleton/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWalkingSkeleton.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly ISuperHeroService _superHeroService;
+        private readonly DataContext _context;
+
+        public SuperHeroController(ISuperHeroService superHeroService, DataContext context)
+        {
+            _superHeroService = superHeroService;
+            _context = context;
+        }
+    }
+}
--- BlazorWalkingSkeleton/BlazorWalkingSkeleton/Server/Program.cs
+++ BlazorWalkingSkeleton/BlazorWalkingSkeleton/Server/Program.cs
@@ -1,6 +1,7 @@
 global using BlazorWalkingSkeleton.Shared;
 global using Microsoft.EntityFrameworkCore;
 global using BlazorWalkingSkeleton.Server.Data;
+global using BlazorWalkingSkeleton.Server.Services.SuperHeroService;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
 
 var app = builder.Build();
 
