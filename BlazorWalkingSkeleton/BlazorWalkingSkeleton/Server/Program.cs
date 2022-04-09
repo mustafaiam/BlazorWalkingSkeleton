4201e39ae28d2300d8a8be385b22cccb2c1bef6d
mustafaiam <mustafaiam.github@gmail.com>
2022-04-09 03:23:16 +0300

    Implemented DataContext
    Implemented the DataContext and connected the DB to the Server project. I need to go to bed because I am falling asleep in the chair, but I will continue tomorrow because right now I literally cannot think.
    P.S. Wow, are you still reading this?! Well you did it, this is the end of this long description.
    THE END

--- BlazorWalkingSkeleton/BlazorWalkingSkeleton/Server/Program.cs
+++ BlazorWalkingSkeleton/BlazorWalkingSkeleton/Server/Program.cs
@@ -1,3 +1,6 @@
+global using BlazorWalkingSkeleton.Shared;
+global using Microsoft.EntityFrameworkCore;
+global using BlazorWalkingSkeleton.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
